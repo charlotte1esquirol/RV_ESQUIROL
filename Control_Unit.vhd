5f7ec7e3decf0ad463a21660ffe9b3b19df0b107
charlotte1esquirol <94962700+charlotte1esquirol@users.noreply.github.com>
2022-06-17 13:33:23 +0200

    Add files via upload

---
+++ Control_Unit.vhd
@@ -0,0 +1,101 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use IEEE.numeric_std.all;
+library work;
+use work.muxs_pkg.all;
+
+
+entity Control_Unit is
+
+generic (DWIDTH: natural := 32  );  -- Number of data bits per input/output
+
+  Port (CLK_A : in STD_LOGIC;
+	RESET_A : in STD_LOGIC;
+	MEM_BUSY : in STD_LOGIC;
+	INSTRUCTION : in STD_LOGIC_VECTOR ( 31 downto 0 );
+	CONTROL_SIG : out STD_LOGIC_VECTOR ( 31 downto 0));
+
+
+end Control_Unit;
+
+architecture Behavioral of Control_Unit is
+
+	constant N : natural := 32;
+
+	component InstrNumber
+
+		Port (FUNC12 : in STD_LOGIC;
+			FUNC7 : in STD_LOGIC;
+			FUNC3 : in STD_LOGIC_VECTOR ( 2 downto 0 );
+			OPCODE : in STD_LOGIC_VECTOR ( 4 downto 0 );
+			NUMBER : out STD_LOGIC_VECTOR ( 7 downto 0));
+
+	end component InstrNumber;
+
+	component AddressCounter
+
+		  Port (WAITMEM : in STD_LOGIC;
+			MEMBUSY : in STD_LOGIC;
+			LOAD : in STD_LOGIC;
+			LOAD_VAL : in STD_LOGIC_VECTOR ( 7 downto 0);
+			VALU : out STD_LOGIC_VECTOR ( 7 downto 0);
+			RESTART : in STD_LOGIC;
+			RESET : in STD_LOGIC;
+			CLK : in STD_LOGIC );
+
+	end component AddressCounter;
+
+
+	component MUX2TO1 
+
+	generic ( DWIDTH: natural := 32  );  -- Number of data bits per input/output
+
+		Port (  SEL: in std_logic;
+			DATA0: in std_logic_vector (DWIDTH-1 downto 0);
+			DATA1: in std_logic_vector (DWIDTH-1 downto 0);
+			RESULT: out std_logic_vector (DWIDTH-1 downto 0));
+
+	end component MUX2TO1;
+
+	component ROM_Memory 
+
+  		Port (  ADDRESS : in STD_LOGIC_VECTOR (7 downto 0);
+        		DATA : out STD_LOGIC_VECTOR (31 downto 0);
+         		WAITMEM : in STD_LOGIC;
+			MEMBUSY : in STD_LOGIC;
+         		CLK : in STD_LOGIC);
+
+	end component ROM_Memory ;
+
+
+
+
+signal INUM, uCYCLE, uADDR, RESULTMUX1, ZERO: STD_LOGIC_VECTOR ( 7 downto 0 );
+
+signal uINSTR : STD_LOGIC_VECTOR ( 31 downto 0 );
+
+alias sFUNC3 : std_logic_vector(2 downto 0) is INSTRUCTION(14 downto 12);
+alias sFUNC7 : std_logic is INSTRUCTION( 30 );
+alias sFUNC12 : std_logic is INSTRUCTION( 20 );
+alias sOPCODE : std_logic_vector is INSTRUCTION( 6 downto 2 );
+alias EOF : std_logic is uINSTR ( 30 );
+alias EOI : std_logic is uINSTR ( 31 );
+alias sWAITMEM : std_logic is uINSTR ( 0 );
+
+
+
+
+begin
+
+	ZERO<="00000000";
+	
+	instrnumber1 : InstrNumber port map (FUNC3=>sFUNC3, FUNC7=>sFUNC7, FUNC12 =>sFUNC12, OPCODE=>sOPCODE, NUMBER=>INUM );
+	addresscounter1 : AddressCounter port map (WAITMEM=>sWAITMEM, MEMBUSY=>MEM_BUSY, LOAD=>EOF, LOAD_VAL=>INUM, VALU=>uCYCLE, RESTART => EOI, RESET=>RESET_A, CLK=>CLK_A );
+	mux1 : MUX2TO1 generic map (DWIDTH=>N) port map (SEL=>EOF, DATA0=>uCYCLE, DATA1=>INUM, RESULT=>RESULTMUX1);
+	mux2 : MUX2TO1 generic map (DWIDTH=>N) port map (SEL=>EOI, DATA0=>RESULTMUX1, DATA1=>ZERO, RESULT=>uADDR);
+	rommemory : ROM_Memory port map (ADDRESS=>uADDR, DATA=>CONTROL_SIG, WAITMEM=>sWAITMEM, MEMBUSY=>MEM_BUSY, CLK=>CLK_A );
+
+	CONTROL_SIG<=uINSTR;
+	
+end architecture Behavioral;
+
---
+++ Control_Unit_tb.vhd
@@ -0,0 +1,180 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+library work;
+use work.muxs_pkg.all;
+
+
+entity Control_Unit_tb is
+
+end Control_Unit_tb; 
+
+
+architecture Behavioral of Control_Unit_tb is
+
+component Control_Unit
+
+generic (DWIDTH: natural := 32  );  -- Number of data bits per input/output
+
+  Port (CLK_A : in STD_LOGIC;
+	RESET_A : in STD_LOGIC;
+	MEM_BUSY : in STD_LOGIC;
+	INSTRUCTION : in STD_LOGIC_VECTOR ( 31 downto 0 );
+	CONTROL_SIG : out STD_LOGIC_VECTOR ( 31 downto 0));
+
+
+end Control_Unit;
+
+end component; 
+
+     
+
+
+signal sINSTR, sCONTROL_SIG: STD_LOGIC_VECTOR(31 downto 0);
+
+signal sCLK, sRESET, sMEMBUSY: STD_LOGIC; 
+
+
+
+begin 
+
+
+
+UUT : Control_Unit  port map ( INSTRUCTION=>sINSTR, CONTROL_SIG=>sCONTROL_SIG, CLK_A=>sCLK, MEM_BUSY=>sMEMBUSY, RESET_A=>sRESET );
+
+process 
+
+    begin
+
+    sCLK <='0';
+
+        wait for 10 ns;
+
+    sCLK <='1';
+
+        wait for 10 ns;
+
+    end process;
+
+
+process
+
+    begin 
+
+	-- On reset pour bien demarrer
+
+	sRESET<='1';
+	wait for 30 ns;
+
+	-- On initialise
+
+	sRESET<='0';
+	sMEMBUSY<='1';
+	sINSTR<="
+
+	sRS1<="00000000000000000000000000000001"; 
+	sRS2<="00000000000000000000000000000010"; 
+	sI<="10000000000000000000000000000000"; 
+	sU<="01000000000000000000000000000000"; 
+	sS<="00100000000000000000000000000000"; 
+	sPC<="11111000000000000000000000000000";
+
+	-- On initialise les valeurs des selecteurs toutes à 0
+
+	sSEL1<='0';
+	sSEL2<="00";
+	sSELOP<="0000";
+
+	wait for 30 ns;
+
+	-- On change SEL1 à 1
+
+	sSEL1<='1';
+	wait for 30 ns;
+
+	-- On remet SEL1 à 0 et change SEL2 à 1
+
+	sSEL2<="01";
+	wait for 30 ns;
+
+	-- On change encore SEL2 à 2
+
+	sSEL2<="10";
+	wait for 30 ns;
+
+	-- On change encore SEL2 à 3
+
+	sSEL2<="10";
+	wait for 30 ns;
+
+	-- On remet SEL1 à 0 et SEL2 à 0 et change SELOP à 1
+
+	sSELOP<="0001";
+	wait for 30 ns;
+
+	-- On change encore SELOP à 2
+
+	sSELOP<="0010";
+	wait for 30 ns;
+
+	-- On change encore SELOP à 3
+
+	sSELOP<="0011";
+	wait for 30 ns;
+
+	-- On change encore SELOP à 4
+
+	sSELOP<="0100";
+	wait for 30 ns;
+
+	-- On change encore SELOP à 5
+
+	sSELOP<="0101";
+	wait for 30 ns;
+
+	-- On change encore SELOP à 6
+
+	sSELOP<="0110";
+	wait for 30 ns;
+
+	-- On change encore SELOP à 7
+
+	sSELOP<="0111";
+	wait for 30 ns;
+
+	-- On change encore SELOP à 8
+
+	sSELOP<="1000";
+	wait for 30 ns;
+
+	-- On change encore SELOP à 9
+
+	sSELOP<="1001";
+	wait for 30 ns;
+
+	-- On change encore SELOP à 10
+
+	sSELOP<="1010";
+	wait for 30 ns;
+
+	-- On change encore SELOP à 11
+
+	sSELOP<="1011";
+	wait for 30 ns;
+
+	-- On vérifie que ca marche pour tout
+
+	sSEL1<='1';
+	sSEL2<="11";
+	wait for 30 ns;
+
+	sSEL2<="10";
+	sSELOP<="0110";
+	wait for 200 ns;
+
+
+	
+   
+end process;
+
+end Behavioral;
